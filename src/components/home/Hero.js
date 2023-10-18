c85eded361f9362ee0a7c613fca03e9eb4872208
T31K <t31kmunwong@gmail.com>
2023-10-18 15:23:34 +0800

    test

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -1,12 +1,21 @@
+import { BsStars}  from 'react-icons/bs'
+import Image from 'next/image'
+const videoSrc = "/hero.mp4"
 function Hero(){
     return(
-        <div className="hero py-32 bg-base-100">
+        <div className="hero px-32 bg-base-100">
             <div className="hero-content max-w-5xl flex-col lg:flex-row-reverse">
-                <img src="https://picsum.photos/seed/picsum/500/300" className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                <h1 className="text-5xl font-bold">Box Office News!</h1>
-                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                <button className="btn btn-primary">Get Started</button>
+                <div class="iphone-container scale-[80%] lg:pl-24">
+                    <div className="iphone-x ">
+                    <video autoplay loop muted src={videoSrc} playsInline className="video-content rounded-[35px] h-[780px] w-[400px]"></video>
+
+  
+</div>
+                </div>                
+            <div>
+                <h1 className="text-5xl font-extrabold">Count calories <br />with  <BsStars className='inline'/>AI!</h1>
+                <p className="py-6">Wave your phone over your food, snap a photo & viola!</p>
+                <button className="btn btn-primary rounded-full">Join waitlist </button>
                 </div>
             </div>
         </div>
